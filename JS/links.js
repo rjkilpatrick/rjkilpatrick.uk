486a238c97164ed5017b2bc4ce83234d54d8b544
John Kilpatrick <john.kilpatrick@outlook.com>
2019-08-15 17:06:58 +0100

    Fixed missing font-awesome dependancies

--- JS/links.js
+++ JS/links.js
@@ -9,30 +9,30 @@ let links = (function() {
   let pub = {};
 
   /**
-   * Finds all h2 tags and attaches clickable permalinks to them
+   * Finds all h2 tags in .post and attaches clickable permalinks to them
    */
   pub.attach = function() {
-    let headers = document.getElementsByClassName('post')[0].getElementsByTagName('h2');
-    for (let header of headers) {
-      header.setAttribute("id", header.textContent);
-
-      let anchor = document.createElement("a");
-      anchor.setAttribute("href", "#" + header.textContent);
-      /* Use Chain symbol */
-      // let linkSymbol = document.createElement("i");
-      // linkSymbol.setAttribute("class", "fa fa-link");
-      // anchor.appendChild(linkSymbol);
-      /* Use Hash symbol */
-      anchor.innerHTML = "&#35;";
-      anchor.setAttribute("href", "#" + header.textContent);
-
-      anchor.setAttribute("class", "anchor");
-      header.appendChild(anchor);
+    let posts = document.getElementsByClassName("post");
+    for (let post of posts) {
+      let headers = post.getElementsByTagName("h2");
+      for (let header of headers) {
+        addLink(header);
+      }
     }
-    console.log(window.location.href);
   };
 
+  function addLink(header) {
+    header.setAttribute("id", header.textContent); // Creates linkable id
+    let anchor = document.createElement("a");
+    anchor.setAttribute("href", "#" + header.textContent);
+    let linkSymbol = document.createElement("i");
+    linkSymbol.setAttribute("class", "fa fa-link");
+    anchor.appendChild(linkSymbol);
+    anchor.setAttribute("class", "anchor");
+    header.appendChild(anchor);
+  }
+
   return pub;
-}());
+})();
 
 links.attach(); // Already deferred by script tag
