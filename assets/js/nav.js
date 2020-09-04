0f784f439bc98d3d504612c5ed6581bdb014e5f8
John Kilpatrick <john.kilpatrick@outlook.com>
2020-09-04 20:19:01 +0100

    :sparkles: Adds mobile navigation menu
    Working hamburger style menu added
    Uses flexbox for centering items in menu
    Items are dynamically added from `_data/navigation.yml`
    
    TODO: Currenly the js for this is included in the navigation html
    section. Move this when the landing page is finalised/move into layouts

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,21 +1,16 @@
+const toggle = document.querySelector(".toggle-container");
+const modal = document.querySelector(".modal");
+
 function toggleNav(event) {
   event.preventDefault();
 
-  /* Find all .nav-item and toggle `active' class */
-  let items = document.querySelectorAll(".nav-item");
-  for (const item of items) {
-    item.classList.toggle("active");
-  }
+  modal.classList.toggle("active");
 
-  /* Handle changing hamburger to close button */
+  // Handle changing hamburger to close button
   let hamburger = this.querySelector("i");
   hamburger.classList.toggle("fa-bars"); // Remove / Add Open menu symbol
   hamburger.classList.toggle("fa-times"); // Remove / Add Close menu symbol
 }
 
-document
-  .querySelector(".hamburger-container")
-  .addEventListener("touchstart", toggleNav);
-document
-  .querySelector(".hamburger-container")
-  .addEventListener("click", toggleNav);
+toggle.addEventListener("touchstart", toggleNav);
+toggle.addEventListener("click", toggleNav);
